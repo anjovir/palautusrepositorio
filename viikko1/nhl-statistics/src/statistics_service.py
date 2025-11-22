c760db13e340b36175b340e8dff748ff074b1f23
anjovir <avirtanen@tutanota.com>
2025-11-22 11:08:06 +0200

    Updated nhl-statistic tests

--- viikko1/nhl-statistics/src/statistics_service.py
+++ viikko1/nhl-statistics/src/statistics_service.py
@@ -1,10 +1,9 @@
 from player_reader import PlayerReader
 
 
-
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, preader):
+        reader = preader
 
         self._players = reader.get_players()
 
---
+++ viikko1/nhl-statistics/src/tests/statistics_service_test.py
@@ -0,0 +1,36 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),  #  4+12 = 16
+            Player("Lemieux", "PIT", 45, 54), # 45+54 = 99
+            Player("Kurri",   "EDM", 37, 53), # 37+53 = 90
+            Player("Yzerman", "DET", 42, 56), # 42+56 = 98
+            Player("Gretzky", "EDM", 35, 89)  # 35+89 = 124
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search_found(self):
+        player = self.stats.search("Semenko")
+        assert player.name == "Semenko"
+
+    def test_search_not_found(self):
+        player = self.stats.search("Heinonen")
+        assert player is None
+    
+    def test_team(self):
+        team = self.stats.team("PIT")
+        assert team == [self.stats.search("Lemieux")]
+    
+    def test_top(self):
+        player1 = self.stats.search("Gretzky")
+        assert self.stats.top(0)[0] == player1
